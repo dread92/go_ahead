a4e8c8139e7aa226992bda043d3ff3ba7e7fb80b
David Read <dread@Davids-MacBook-Pro.local>
2017-07-26 20:16:43 -0400

    app.yaml and simple application that returns your ip

---
+++ goahead.go
@@ -0,0 +1,14 @@
+package goahead
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/ip", handler)
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	header := r.RemoteAddr
+	fmt.Fprint(w, header)
+}
